f3b76ecf00345397c29181eeb6404e5b4c9f1c69
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-12-13 22:48:13 +0100

    Square greater than 20

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Write a query that returns list of numbers and their squares only if square is greater than 20
+
+namespace program{
+    class SquareTask{
+        public static void Main (string [] arg){
+            int [] numbers = {7,2,30};
+            
+            IEnumerable<int> tabNb =
+            from number in numbers
+            where number*number > 20 
+            select number;
+
+            foreach (int i in tabNb)
+            {
+                int square = i*i;
+                Console.WriteLine(i + " - " + square); 
+            }
+
+
+        }
+    }
+}
+
+
+
+
+//Expected input and output
+//[7, 2, 30] → 7 - 49, 30 - 900
